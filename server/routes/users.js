5b9d4970eac85b0cabf82f92a5e8a1fb907acf5a
niteshnandak <kniteshnanda@gmail.com>
2024-07-25 19:48:39 +0530

    methods for user are created in controller and routers and Added Middleware

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,21 @@
 import express from "express"
-import {  } from "../controllers/usercontroller.js";
+import { update, remove, getUser, subscribe, unsubscribe, like, dislike } from "../controllers/usercontroller.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+router.put("/:id", verifyToken, update)
+
+router.delete("/:id", remove)
+
+router.get("/find/:id", getUser)
+
+router.put("/sub/:id", subscribe)
+
+router.put("/unsub/:id", unsubscribe)
+
+router.put("/like/:videoId", like)
+
+router.put("/dislike/:videoId", dislike)
 
 export default router;
