f901ebd562b6b0080e50b3ed28b649fa176ec392
niteshnandak <kniteshnanda@gmail.com>
2024-07-27 13:48:37 +0530

    Videos Part Completely Done. Search, trend, tags methods completed

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,7 +1,6 @@
 import express from "express"
-import { addVideo, addView, deleteVideo, getVideo, random, trend, updateVideo } from "../controllers/videocontroller.js";
+import { addVideo, addView, deleteVideo, getByTags, getVideo, random, search, sub, trend, updateVideo } from "../controllers/videocontroller.js";
 import { verifyToken } from "../verifyToken.js";
-import { subscribe } from "../controllers/usercontroller.js";
 
 const router = express.Router();
 
@@ -14,6 +13,9 @@ router.put("/view/:id", addView)
 // to display the trending videos, random videos, subscribed channel videos
 router.get("/trend/", trend)
 router.get("/random/", random)
-router.get("/sub/", verifyToken, subscribe)
+router.get("/sub/", verifyToken, sub)
+router.get("/tags/", getByTags)
+router.get("/search/", search)
+
 
 export default router;
